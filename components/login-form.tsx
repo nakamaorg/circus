0473b1af466ab5b38c9ff62e91675c791e356c0c
eoussama <oussama.essamadi@gmail.com>
2025-01-01 22:54:39 +0100

    feat: removed reset link, #7

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -60,12 +60,6 @@ export function LoginForm({
                   <span className="sr-only">Login with Discord</span>
                 </Button>
               </div>
-              <div className="text-center text-sm">
-                Don&apos;t have an account?{" "}
-                <a href="#" className="underline underline-offset-4">
-                  Sign up
-                </a>
-              </div>
             </div>
           </form>
           <div className="relative hidden bg-muted md:block">

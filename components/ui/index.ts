9e0014d1be0732659988457f6f1c3e087ca18aeb
eoussama <oussama_gtacj@live.fr>
2025-08-01 01:13:23 +0100

    feat: add exports for DropdownMenu components in index.ts

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,3 +1,21 @@
+export { Avatar, AvatarFallback, AvatarImage } from "./avatar";
 export { Button } from "./button";
 export { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./card";
 export { DiscordIcon } from "./discord-icon";
+export {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "./dropdown-menu";

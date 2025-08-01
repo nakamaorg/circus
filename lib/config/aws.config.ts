ebb7ca6478505da6970147ba4a99f7fa8ac96eee
eoussama <oussama_gtacj@live.fr>
2025-08-01 13:44:42 +0100

    feat: add S3 client configuration to AWS setup

--- lib/config/aws.config.ts
+++ lib/config/aws.config.ts
@@ -1,6 +1,7 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { S3Client } from "@aws-sdk/client-s3";
 import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
-import { env } from "@/lib/config/env.config";
+import { env } from "../config/env.config";
 
 
 
@@ -12,8 +13,21 @@ const dynamoDBClient = new DynamoDBClient({
   },
 });
 
+const s3Client = new S3Client({
+  region: env.AWS_REGION,
+  credentials: {
+    accessKeyId: env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
+  },
+});
+
 export const AWS_TABLES = {
   USERS: "nakamaorg-users",
 } as const;
 
+export const AWS_BUCKETS = {
+  NAKAMAORG: "nakamaorg",
+} as const;
+
 export const docClient = DynamoDBDocumentClient.from(dynamoDBClient);
+export const s3 = s3Client;

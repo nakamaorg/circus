b4341c777911eba38003aaa85740ee6273137c8c
eoussama <oussama_gtacj@live.fr>
2025-08-01 03:28:15 +0100

    feat: remove .eslintignore file and consolidate ignore patterns in eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -2,4 +2,22 @@ import dx from "@eoussama/dx";
 
 
 
-export default dx();
+export default dx({
+  ignores: [
+    // Dependencies
+    "node_modules/**",
+    ".pnpm-store/**",
+
+    // Build outputs
+    ".next/**",
+    "out/**",
+    "dist/**",
+
+    // Environment files
+    ".env*",
+
+    // Other
+    ".vercel/**",
+    "coverage/**",
+  ],
+});

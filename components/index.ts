55614cf45fce9cf14a29d52f22d12ae6f4876065
eoussama <oussama_gtacj@live.fr>
2025-08-01 01:14:47 +0100

    feat: add exports for DashboardHeader, DashboardLayout, Sidebar, and UserMenu components

--- components/index.ts
+++ components/index.ts
@@ -1,2 +1,6 @@
 export { CircusLogo } from "./circus-logo";
+export { DashboardHeader } from "./dashboard-header";
+export { DashboardLayout } from "./dashboard-layout";
 export { DiscordLoginButton } from "./discord-login-button";
+export { Sidebar } from "./sidebar";
+export { UserMenu } from "./user-menu";

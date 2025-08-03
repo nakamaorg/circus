5872dd5b7727dbe2538bb7e26ae15378dbe28a74
eoussama <oussama_gtacj@live.fr>
2025-08-03 13:28:31 +0100

    feat: enhance dropdown menu animation speed for improved user experience

--- components/ui/dropdown-menu.tsx
+++ components/ui/dropdown-menu.tsx
@@ -68,7 +68,7 @@ const DropdownMenuContent = React.forwardRef<
       ref={ref}
       sideOffset={sideOffset}
       className={cn(
-        "animate__animated animate__bounceIn z-50 min-w-[8rem] overflow-hidden rounded-none border-4 border-black bg-white p-1 text-black shadow-[8px_8px_0px_0px_rgba(0,0,0,1)] data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+        "animate__animated animate__bounceIn animate__faster z-50 min-w-[8rem] overflow-hidden rounded-none border-4 border-black bg-white p-1 text-black shadow-[8px_8px_0px_0px_rgba(0,0,0,1)] data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
         className,
       )}
       {...props}
